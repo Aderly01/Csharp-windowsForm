ea35bb632349c3e4368c7fdddba46e5cf43edd39
Aderly01 <60637099+Aderly01@users.noreply.github.com>
2022-10-02 11:44:19 -0600

    Add files via upload

---
+++ DesktopApp/DesktopApp/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace DesktopApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TextBox txtServidor;
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPuerto = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDB = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            txtServidor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Servidor:";
+            // 
+            // txtServidor
+            // 
+            txtServidor.Location = new System.Drawing.Point(113, 31);
+            txtServidor.Name = "txtServidor";
+            txtServidor.Size = new System.Drawing.Size(100, 20);
+            txtServidor.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Puerto:";
+            // 
+            // txtPuerto
+            // 
+            this.txtPuerto.Location = new System.Drawing.Point(113, 77);
+            this.txtPuerto.Name = "txtPuerto";
+            this.txtPuerto.Size = new System.Drawing.Size(100, 20);
+            this.txtPuerto.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Usuario:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(113, 120);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password:";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(113, 168);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(100, 20);
+            this.txtPass.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "DB:";
+            // 
+            // txtDB
+            // 
+            this.txtDB.Location = new System.Drawing.Point(113, 211);
+            this.txtDB.Name = "txtDB";
+            this.txtDB.Size = new System.Drawing.Size(100, 20);
+            this.txtDB.TabIndex = 9;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(194, 278);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(86, 36);
+            this.btnConectar.TabIndex = 10;
+            this.btnConectar.Text = "CONECTAR";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 347);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtDB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPuerto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(txtServidor);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cliente MySql";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPuerto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDB;
+        private System.Windows.Forms.Button btnConectar;
+    }
+}
+
---
+++ DesktopApp/DesktopApp/Form1.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            String servidor = txtServidor.Text;
+            String puerto = txtPuerto.Text;
+            String usuario = txtUsuario.Text;
+            String password = txtPass.Text;
+            String db = txtDB.Text;
+
+            String cadenaConexion = $"Database={db}; Data Source={servidor};Port={puerto};User Id={usuario};Password={password};";
+
+            MySqlConnection conexionBD = new MySqlConnection(cadenaConexion);
+            MySqlDataReader reader = null;
+            String data = null;
+
+            try
+            {
+                String consulta = "SHOW DATABASES";
+                MySqlCommand comando = new MySqlCommand(consulta);
+                comando.Connection = conexionBD;
+                conexionBD.Open();
+                reader = comando.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    data += reader.GetString(0) + "\n";
+                }
+                MessageBox.Show(data);
+            }
+            catch (MySqlException error)
+            {
+                MessageBox.Show("Error: " + error.Message);
+            }
+            finally
+            {
+                conexionBD.Close();
+            }
+
+        }
+    }
+}
